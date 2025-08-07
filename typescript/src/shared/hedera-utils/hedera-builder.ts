2398bf1f785c1b0db0d4cfa40ebb05711481fa08
NikhilRaikwar <raikwarnikhil80@gmail.com>
2025-08-07 14:49:25 +0530

    add files

---
+++ typescript/src/shared/hedera-utils/hedera-builder.ts
@@ -0,0 +1,75 @@
+import {
+  TokenAirdropTransaction,
+  TokenCreateTransaction,
+  TopicCreateTransaction,
+  TopicMessageSubmitTransaction,
+  TransferTransaction,
+  ContractExecuteTransaction,
+  TokenMintTransaction,
+} from '@hashgraph/sdk';
+import {
+  airdropFungibleTokenParametersNormalised,
+  createFungibleTokenParametersNormalised,
+  createNonFungibleTokenParametersNormalised,
+  mintFungibleTokenParametersNormalised,
+  mintNonFungibleTokenParametersNormalised,
+} from '@/shared/parameter-schemas/hts.zod';
+import z from 'zod';
+import { transferHbarParametersNormalised } from '@/shared/parameter-schemas/has.zod';
+import {
+  createTopicParametersNormalised,
+  submitTopicMessageParametersNormalised,
+} from '@/shared/parameter-schemas/hcs.zod';
+import { contractExecuteTransactionParametersNormalised } from '../parameter-schemas/hscs.zod';
+
+export default class HederaBuilder {
+  static createFungibleToken(
+    params: z.infer<ReturnType<typeof createFungibleTokenParametersNormalised>>,
+  ) {
+    return new TokenCreateTransaction(params);
+  }
+
+  static createNonFungibleToken(
+    params: z.infer<ReturnType<typeof createNonFungibleTokenParametersNormalised>>,
+  ) {
+    return new TokenCreateTransaction(params);
+  }
+
+  static transferHbar(params: z.infer<ReturnType<typeof transferHbarParametersNormalised>>) {
+    return new TransferTransaction(params);
+  }
+
+  static airdropFungibleToken(
+    params: z.infer<ReturnType<typeof airdropFungibleTokenParametersNormalised>>,
+  ) {
+    return new TokenAirdropTransaction(params as any);
+  }
+
+  static createTopic(params: z.infer<ReturnType<typeof createTopicParametersNormalised>>) {
+    return new TopicCreateTransaction(params);
+  }
+
+  static submitTopicMessage(
+    params: z.infer<ReturnType<typeof submitTopicMessageParametersNormalised>>,
+  ) {
+    return new TopicMessageSubmitTransaction(params);
+  }
+
+  static executeTransaction(
+    params: z.infer<ReturnType<typeof contractExecuteTransactionParametersNormalised>>,
+  ) {
+    return new ContractExecuteTransaction(params);
+  }
+
+  static mintFungibleToken(
+    params: z.infer<ReturnType<typeof mintFungibleTokenParametersNormalised>>,
+  ) {
+    return new TokenMintTransaction(params);
+  }
+
+  static mintNonFungibleToken(
+    params: z.infer<ReturnType<typeof mintNonFungibleTokenParametersNormalised>>,
+  ) {
+    return new TokenMintTransaction(params);
+  }
+}
---
+++ typescript/src/shared/hedera-utils/hedera-parameter-normaliser.ts
@@ -0,0 +1,305 @@
+// optional to use methods in here
+
+import {
+  airdropFungibleTokenParameters,
+  createFungibleTokenParameters,
+  createFungibleTokenParametersNormalised,
+  createNonFungibleTokenParameters,
+  createNonFungibleTokenParametersNormalised,
+  mintFungibleTokenParameters,
+  mintNonFungibleTokenParameters,
+} from '@/shared/parameter-schemas/hts.zod';
+import { transferHbarParameters } from '@/shared/parameter-schemas/has.zod';
+import {
+  createTopicParameters,
+  createTopicParametersNormalised,
+} from '@/shared/parameter-schemas/hcs.zod';
+import { Client, Hbar, PublicKey, TokenSupplyType, TokenType } from '@hashgraph/sdk';
+import { Context } from '@/shared/configuration';
+import z from 'zod';
+import {
+  accountBalanceQueryParameters,
+  accountTokenBalancesQueryParameters,
+} from '@/shared/parameter-schemas/account-query.zod';
+import { IHederaMirrornodeService } from '@/shared/hedera-utils/mirrornode/hedera-mirrornode-service.interface';
+import { toBaseUnit } from '@/shared/hedera-utils/decimals-utils';
+import Long from 'long';
+import { TokenTransferMinimalParams, TransferHbarInput } from '@/shared/hedera-utils/types';
+import { AccountResolver } from '@/shared/utils/account-resolver';
+import { createERC20Parameters } from '../parameter-schemas/erc20.zod';
+import { ethers } from 'ethers';
+
+export default class HederaParameterNormaliser {
+  static async normaliseCreateFungibleTokenParams(
+    params: z.infer<ReturnType<typeof createFungibleTokenParameters>>,
+    context: Context,
+    client: Client,
+    mirrorNode: IHederaMirrornodeService,
+  ) {
+    const defaultAccountId = AccountResolver.getDefaultAccount(context, client);
+
+    const normalized: z.infer<ReturnType<typeof createFungibleTokenParametersNormalised>> = {
+      ...params,
+      supplyType: TokenSupplyType.Finite, // defaults to finite supply
+      autoRenewAccountId: defaultAccountId,
+    };
+
+    const treasuryAccountId = params.treasuryAccountId ?? defaultAccountId;
+
+    if (!treasuryAccountId) {
+      throw new Error('Must include treasury account ID');
+    }
+
+    const supplyTypeString = params.supplyType ?? 'infinite';
+    const supplyType =
+      supplyTypeString === 'finite' ? TokenSupplyType.Finite : TokenSupplyType.Infinite;
+    const decimals = params.decimals ?? 0;
+    const initialSupply = toBaseUnit(params.initialSupply ?? 0, decimals);
+
+    let maxSupply: number | undefined = undefined;
+    if (supplyTypeString === 'finite') {
+      if (!params.maxSupply) {
+        throw new Error('Must include max supply for finite supply type');
+      }
+      maxSupply = toBaseUnit(params.maxSupply, decimals);
+
+      if (initialSupply > maxSupply) {
+        throw new Error(
+          `Initial supply (${initialSupply}) cannot exceed max supply (${maxSupply})`,
+        );
+      }
+    }
+
+    const publicKey =
+      (await mirrorNode.getAccount(defaultAccountId).then(r => r.accountPublicKey)) ??
+      client.operatorPublicKey?.toStringDer();
+
+    if (params.isSupplyKey === true) {
+      normalized.supplyKey = PublicKey.fromString(publicKey);
+    }
+
+    const autoRenewAccountId = defaultAccountId;
+
+    return {
+      ...normalized,
+      treasuryAccountId,
+      supplyType,
+      maxSupply,
+      decimals,
+      initialSupply,
+      autoRenewAccountId,
+    };
+  }
+
+  static async normaliseCreateNonFungibleTokenParams(
+    params: z.infer<ReturnType<typeof createNonFungibleTokenParameters>>,
+    context: Context,
+    client: Client,
+    mirrorNode: IHederaMirrornodeService,
+  ) {
+    const defaultAccountId = AccountResolver.getDefaultAccount(context, client);
+
+    const treasuryAccountId = params.treasuryAccountId || defaultAccountId;
+    if (!treasuryAccountId) throw new Error('Must include treasury account ID');
+
+    const publicKey =
+      (await mirrorNode.getAccount(defaultAccountId).then(r => r.accountPublicKey)) ??
+      client.operatorPublicKey?.toStringDer();
+
+    const maxSupply = params.maxSupply ?? 100;
+    const normalized: z.infer<ReturnType<typeof createNonFungibleTokenParametersNormalised>> = {
+      ...params,
+      treasuryAccountId,
+      maxSupply,
+      supplyKey: PublicKey.fromString(publicKey), // the supply key is mandatory in the case of NFT
+      supplyType: TokenSupplyType.Finite, // NFTs supply must be finite
+      autoRenewAccountId: defaultAccountId,
+      tokenType: TokenType.NonFungibleUnique,
+    };
+
+    return normalized;
+  }
+
+  static normaliseTransferHbar(
+    params: z.infer<ReturnType<typeof transferHbarParameters>>,
+    context: Context,
+    client: Client,
+  ) {
+    const sourceAccountId = AccountResolver.resolveAccount(params.sourceAccountId, context, client);
+
+    const hbarTransfers: TransferHbarInput[] = [];
+
+    let totalTinybars = Long.ZERO;
+
+    for (const transfer of params.transfers) {
+      const amount = new Hbar(transfer.amount);
+
+      if (amount.isNegative() || amount.toTinybars().equals(Long.ZERO)) {
+        throw new Error(`Invalid transfer amount: ${transfer.amount}`);
+      }
+
+      totalTinybars = totalTinybars.add(amount.toTinybars());
+
+      hbarTransfers.push({
+        accountId: transfer.accountId,
+        amount,
+      });
+    }
+
+    hbarTransfers.push({
+      accountId: sourceAccountId,
+      amount: Hbar.fromTinybars(totalTinybars.negate()),
+    });
+
+    return {
+      hbarTransfers,
+      transactionMemo: params.transactionMemo,
+    };
+  }
+
+  static async normaliseAirdropFungibleTokenParams(
+    params: z.infer<ReturnType<typeof airdropFungibleTokenParameters>>,
+    context: Context,
+    client: Client,
+    mirrorNode: IHederaMirrornodeService,
+  ) {
+    const sourceAccountId = AccountResolver.resolveAccount(params.sourceAccountId, context, client);
+
+    const tokenDetails = await mirrorNode.getTokenDetails(params.tokenId);
+    const tokenDecimals = parseInt(tokenDetails.decimals, 10);
+
+    const tokenTransfers: TokenTransferMinimalParams[] = [];
+    let totalAmount = Long.ZERO;
+
+    for (const recipient of params.recipients) {
+      const amountRaw = Number(recipient.amount);
+
+      if (amountRaw <= 0) {
+        throw new Error(`Invalid recipient amount: ${recipient.amount}`);
+      }
+
+      const amount = Long.fromString(toBaseUnit(amountRaw, tokenDecimals).toString());
+
+      totalAmount = totalAmount.add(amount);
+
+      tokenTransfers.push({
+        tokenId: params.tokenId,
+        accountId: recipient.accountId,
+        amount,
+      });
+    }
+
+    // Sender negative total
+    tokenTransfers.push({
+      tokenId: params.tokenId,
+      accountId: sourceAccountId,
+      amount: totalAmount.negate(),
+    });
+
+    return {
+      tokenTransfers,
+    };
+  }
+
+  static async normaliseCreateTopicParams(
+    params: z.infer<ReturnType<typeof createTopicParameters>>,
+    context: Context,
+    client: Client,
+    mirrorNode: IHederaMirrornodeService,
+  ) {
+    const defaultAccountId = AccountResolver.getDefaultAccount(context, client);
+    const normalised: z.infer<ReturnType<typeof createTopicParametersNormalised>> = {
+      ...params,
+      autoRenewAccountId: defaultAccountId,
+    };
+
+    if (params.isSubmitKey) {
+      const publicKey =
+        (await mirrorNode.getAccount(defaultAccountId).then(r => r.accountPublicKey)) ??
+        client.operatorPublicKey?.toStringDer();
+      if (!publicKey) {
+        throw new Error('Could not determine default account ID for submit key');
+      }
+      normalised.submitKey = PublicKey.fromString(publicKey);
+    }
+
+    return normalised;
+  }
+
+  static normaliseHbarBalanceParams(
+    params: z.infer<ReturnType<typeof accountBalanceQueryParameters>>,
+    context: Context,
+    client: Client,
+  ) {
+    const accountId = AccountResolver.resolveAccount(params.accountId, context, client);
+    return {
+      ...params,
+      accountId,
+    };
+  }
+
+  static normaliseAccountTokenBalancesParams(
+    params: z.infer<ReturnType<typeof accountTokenBalancesQueryParameters>>,
+    context: Context,
+    client: Client,
+  ) {
+    const accountId = AccountResolver.resolveAccount(params.accountId, context, client);
+    return {
+      ...params,
+      accountId,
+    };
+  }
+
+  static normaliseCreateERC20Params(
+    params: z.infer<ReturnType<typeof createERC20Parameters>>,
+    factoryContractId: string,
+    factoryContractAbi: string[],
+    factoryContractFunctionName: string,
+  ) {
+    // Create interface for encoding
+    const iface = new ethers.Interface(factoryContractAbi);
+
+    // Encode the function call
+    const encodedData = iface.encodeFunctionData(factoryContractFunctionName, [
+      params.tokenName,
+      params.tokenSymbol,
+      params.decimals,
+      params.initialSupply,
+    ]);
+
+    const functionParameters = ethers.getBytes(encodedData);
+
+    return {
+      ...params,
+      contractId: factoryContractId,
+      functionParameters,
+      gas: 3000000, //TODO: make this configurable
+    };
+  }
+
+  static async normaliseMintFungibleTokenParams(
+    params: z.infer<ReturnType<typeof mintFungibleTokenParameters>>,
+    context: Context,
+    mirrorNode: IHederaMirrornodeService,
+  ) {
+    const decimals =
+      (await mirrorNode.getTokenDetails(params.tokenId).then(r => Number(r.decimals))) ?? 0;
+    const baseAmount = toBaseUnit(params.amount, decimals);
+    return {
+      tokenId: params.tokenId,
+      amount: baseAmount,
+    };
+  }
+
+  static normaliseMintNonFungibleTokenParams(
+    params: z.infer<ReturnType<typeof mintNonFungibleTokenParameters>>,
+    _context: Context,
+  ) {
+    const encoder = new TextEncoder();
+    const metadata = params.uris.map(uri => encoder.encode(uri));
+    return {
+      ...params,
+      metadata: metadata,
+    };
+  }
+}
