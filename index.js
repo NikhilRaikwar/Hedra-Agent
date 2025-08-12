c68749e1243c767d7323ae213837308d73ff6687
NikhilRaikwar <raikwarnikhil80@gmail.com>
2025-08-12 23:35:51 +0530

    add files

---
+++ index.js
@@ -0,0 +1,59 @@
+// index.js
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { ChatOpenAI } from '@langchain/openai';
+import { ChatPromptTemplate } from '@langchain/core/prompts';
+import { AgentExecutor, createToolCallingAgent } from 'langchain/agents';
+import { Client, PrivateKey } from '@hashgraph/sdk';
+import { HederaLangchainToolkit, coreQueriesPlugin } from 'hedera-agent-kit';
+
+
+async function main() {
+  // Initialise OpenAI LLM
+  const llm = new ChatOpenAI({
+    model: 'gpt-4o-mini',
+  });
+
+  // Hedera client setup (Testnet by default)
+  const client = Client.forTestnet().setOperator(
+    process.env.ACCOUNT_ID,
+    PrivateKey.fromStringDer(process.env.PRIVATE_KEY),
+  ); // get these from https://portal.hedera.com
+
+  const hederaAgentToolkit = new HederaLangchainToolkit({
+    client,
+    configuration: {
+      plugins: [coreQueriesPlugin] // all our core plugins here https://github.com/hedera-dev/hedera-agent-kit/tree/main/typescript/src/plugins
+    },
+  });
+  
+  // Load the structured chat prompt template
+  const prompt = ChatPromptTemplate.fromMessages([
+    ['system', 'You are a helpful assistant'],
+    ['placeholder', '{chat_history}'],
+    ['human', '{input}'],
+    ['placeholder', '{agent_scratchpad}'],
+  ]);
+
+  // Fetch tools from toolkit
+  const tools = hederaAgentToolkit.getTools();
+
+  // Create the underlying agent
+  const agent = createToolCallingAgent({
+    llm,
+    tools,
+    prompt,
+  });
+  
+  // Wrap everything in an executor that will maintain memory
+  const agentExecutor = new AgentExecutor({
+    agent,
+    tools,
+  });
+  
+  const response = await agentExecutor.invoke({ input: "what's my balance?" });
+  console.log(response);
+}
+
+main().catch(console.error);
